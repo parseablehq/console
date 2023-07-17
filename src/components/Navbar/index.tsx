be6ebe59f3c40255955479c94b5120d17f109564
Kartik Gupta <kartikgupta7267@gmail.com>
2023-07-17 19:43:12 +0530

    minor fix

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 import type { NavbarProps as MantineNavbarProps } from '@mantine/core';
-import { Navbar as MantineNavbar, NavLink, Select ,Anchor,Card, Box, Modal ,Text ,Image} from '@mantine/core';
-import { IconNews, IconCodeCircle2, IconCheck,  IconFileAlert, IconReload, IconHelpCircle, IconLogout, IconUser } from '@tabler/icons-react';
+import { Navbar as MantineNavbar, NavLink, Select, Anchor, Card, Box, Modal, Text, Image } from '@mantine/core';
+import { IconNews, IconCodeCircle2, IconCheck, IconFileAlert, IconReload, IconHelpCircle, IconLogout, IconUser } from '@tabler/icons-react';
 import { FC, useEffect, useState } from 'react';
 import docImage from '@/assets/images/doc.webp';
 import githubLogo from '@/assets/images/github-logo.webp';
@@ -32,8 +32,8 @@ const Navbar: FC<NavbarProps> = (props) => {
 	const [searchValue, setSearchValue] = useState("");
 	const { classes } = useNavbarStyles();
 	const { container, linkBtnActive, linkBtn,
-		 selectStreambtn ,streamsBtn ,lowerContainer ,
-		  actionBtn, helpTitle, helpDescription ,userBtn} = classes;
+		selectStreambtn, streamsBtn, lowerContainer,
+		actionBtn, helpTitle, helpDescription, userBtn } = classes;
 	const { streamName } = useParams();
 	const nav = useNavigate();
 	const [, , removeCredentials] = useLocalStorage({ key: 'credentials' });
@@ -70,20 +70,19 @@ const Navbar: FC<NavbarProps> = (props) => {
 		if (streamName) {
 			setActiveStream(streamName);
 			setSearchValue(streamName);
-			if (streamName !== subLogQuery.get().streamName) {
-				const now = dayjs();
-				subLogQuery.set((state) => {
-					state.streamName = streamName || '';
-					state.startTime = now.subtract(DEFAULT_FIXED_DURATIONS.milliseconds, 'milliseconds').toDate();
-					state.endTime = now.toDate();
-				});
-				subLogSelectedTimeRange.set(DEFAULT_FIXED_DURATIONS.name);
-			}
+			const now = dayjs();
+			subLogQuery.set((state) => {
+				state.streamName = streamName || '';
+				state.startTime = now.subtract(DEFAULT_FIXED_DURATIONS.milliseconds, 'milliseconds').toDate();
+				state.endTime = now.toDate();
+			});
+			subLogSelectedTimeRange.set(DEFAULT_FIXED_DURATIONS.name);
+			subRefreshInterval.set(null);
 		}
 		else if (streams && Boolean(streams.length)) {
 			navigate(`/${streams[0].name}/logs`);
 		}
-	}, [streams ,location]);
+	}, [streams, location]);
 
 	const handleChange = (value: string) => {
 		setActiveStream(value);
@@ -142,12 +141,12 @@ const Navbar: FC<NavbarProps> = (props) => {
 
 
 	return (
-		<MantineNavbar {...props} withBorder zIndex={1} hiddenBreakpoint={window.outerWidth+20} hidden={isSubNavbarOpen}>
+		<MantineNavbar {...props} withBorder zIndex={1} hiddenBreakpoint={window.outerWidth + 20} hidden={isSubNavbarOpen}>
 			<MantineNavbar.Section grow className={container}>
 				<NavLink label="Streams" icon={<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-					<path d="M19 5C19 7.21 14.97 9 10 9C5.03 9 1 7.21 1 5M19 5C19 2.79 14.97 1 10 1C5.03 1 1 2.79 1 5M19 5V10M1 5V10M19 10C19 12.21 14.97 14 10 14C5.03 14 1 12.21 1 10M19 10V15C19 17.21 14.97 19 10 19C5.03 19 1 17.21 1 15V10" stroke="#211F1F" strokeLinecap="round" strokeLinejoin='round'/>
+					<path d="M19 5C19 7.21 14.97 9 10 9C5.03 9 1 7.21 1 5M19 5C19 2.79 14.97 1 10 1C5.03 1 1 2.79 1 5M19 5V10M1 5V10M19 10C19 12.21 14.97 14 10 14C5.03 14 1 12.21 1 10M19 10V15C19 17.21 14.97 19 10 19C5.03 19 1 17.21 1 15V10" stroke="#211F1F" strokeLinecap="round" strokeLinejoin='round' />
 				</svg>
-				} className={streamsBtn}   />
+				} className={streamsBtn} />
 				<Select
 					placeholder="Pick one"
 					onChange={(value) => handleChange(value || "")}
@@ -178,7 +177,7 @@ const Navbar: FC<NavbarProps> = (props) => {
 				{error && <NavLink label="Retry" icon={<IconReload size="1rem" stroke={1.5} />} component="button" onClick={getData} sx={{ paddingLeft: 0 }} />}
 			</MantineNavbar.Section>
 			<MantineNavbar.Section className={lowerContainer}>
-				<NavLink label={username} icon={<IconUser size="1.5rem" stroke={1.5} />}  className={userBtn} component="a"  />
+				<NavLink label={username} icon={<IconUser size="1.5rem" stroke={1.5} />} className={userBtn} component="a" />
 				<NavLink label="Help" icon={<IconHelpCircle size="1.5rem" stroke={1.5} />} className={actionBtn} component="a" onClick={open} />
 				<NavLink label="Log out" icon={<IconLogout size="1.5rem" stroke={1.5} />} className={actionBtn} component="a" onClick={onSignOut} />
 			</MantineNavbar.Section>
