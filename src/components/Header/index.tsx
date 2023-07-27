0094611589ff455e3f0e0eb9035fcb884e7199f4
Kartik Gupta <kartikgupta7267@gmail.com>
2023-07-27 17:00:06 +0530

    toogle removed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,42 +2,42 @@ import logoInvert from '@/assets/images/brand/logo-invert.svg';
 import { HOME_ROUTE } from '@/constants/routes';
 import { HEADER_HEIGHT } from '@/constants/theme';
 import type { HeaderProps as MantineHeaderProps } from '@mantine/core';
-import { Box, Image, Header as MantineHeader, Burger } from '@mantine/core';
-import { FC, useEffect } from 'react';
+import { Box, Image, Header as MantineHeader } from '@mantine/core';
+import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { useHeaderStyles } from './styles';
 import SubHeader from './SubHeader';
-import { useHeaderContext } from '@/layouts/MainLayout/Context';
-import useMountedState from '@/hooks/useMountedState';
+// import { useHeaderContext } from '@/layouts/MainLayout/Context';
+// import useMountedState from '@/hooks/useMountedState';
 
 type HeaderProps = Omit<MantineHeaderProps, 'children' | 'height' | 'className'>;
 
 const Header: FC<HeaderProps> = (props) => {
 	const { classes } = useHeaderStyles();
-	const { container, logoContainer, burgerIcon, navContainer, imageSty } = classes;
-	const {
-		state: { subNavbarTogle },
-	} = useHeaderContext();
-	const [isSubNavbarOpen, setIsSubNavbarOpen] = useMountedState(false);
-	useEffect(() => {
-		const listener = subNavbarTogle.subscribe(setIsSubNavbarOpen);
-		return () => {
-			listener();
-		};
-	}, []);
+	const { container, logoContainer, navContainer, imageSty } = classes;
+	// const {
+	// 	state: { subNavbarTogle },
+	// } = useHeaderContext();
+	// const [isSubNavbarOpen, setIsSubNavbarOpen] = useMountedState(false);
+	// useEffect(() => {
+	// 	const listener = subNavbarTogle.subscribe(setIsSubNavbarOpen);
+	// 	return () => {
+	// 		listener();
+	// 	};
+	// }, []);
 
 	return (
 		<MantineHeader {...props} className={container} height={HEADER_HEIGHT} p={0} withBorder>
 			<Box className={logoContainer}>
-				<Link to={HOME_ROUTE} >
+				<Link to={HOME_ROUTE}>
 					<Image className={imageSty} src={logoInvert} height={24} alt="Parseable Logo" />
 				</Link>
-				<Burger
+				{/* <Burger
 					className={burgerIcon}
 					opened={isSubNavbarOpen}
 					onClick={() => subNavbarTogle.set((state) => !state)}
 					size={24}
-				/>
+				/> */}
 			</Box>
 			<Box className={navContainer}>
 				<SubHeader />
