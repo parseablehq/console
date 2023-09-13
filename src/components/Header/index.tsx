53e0f27b3e5da28a1f33d99b3bcea840f106e7b7
Kartik Gupta <kartikgupta7267@gmail.com>
2023-09-13 21:05:50 +0530

    Header Not Showing on no stream fixed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,15 @@ import type { HeaderProps as MantineHeaderProps } from '@mantine/core';
 import { FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HeaderLayout from './Layout';
-import { ConfigHeader, LogsHeader, QueryHeader, StatsHeader, UsersManagementHeader } from './SubHeader';
-import { CONFIG_ROUTE, LOGS_ROUTE, QUERY_ROUTE, STATS_ROUTE, USERS_MANAGEMENT_ROUTE } from '@/constants/routes';
+import { AllRouteHeader, ConfigHeader, LogsHeader, QueryHeader, StatsHeader, UsersManagementHeader } from './SubHeader';
+import {
+	ALL_ROUTE,
+	CONFIG_ROUTE,
+	LOGS_ROUTE,
+	QUERY_ROUTE,
+	STATS_ROUTE,
+	USERS_MANAGEMENT_ROUTE,
+} from '@/constants/routes';
 
 type HeaderProps = Omit<MantineHeaderProps, 'children' | 'height' | 'className'>;
 
@@ -16,6 +23,7 @@ const Header: FC<HeaderProps> = (props) => {
 				<Route path={STATS_ROUTE} element={<StatsHeader />} />
 				<Route path={CONFIG_ROUTE} element={<ConfigHeader />} />
 				<Route path={USERS_MANAGEMENT_ROUTE} element={<UsersManagementHeader />} />
+				<Route path={ALL_ROUTE} element={<AllRouteHeader />} />
 			</Route>
 		</Routes>
 	);
