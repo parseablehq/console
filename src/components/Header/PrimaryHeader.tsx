c28c053fef380caa8aa573f42593d03373351f63
Kartik Gupta <kartikgupta7267@gmail.com>
2023-10-12 18:59:45 +0530

    logo url bug

--- src/components/Header/PrimaryHeader.tsx
+++ src/components/Header/PrimaryHeader.tsx
@@ -4,7 +4,6 @@ import { HEADER_HEIGHT } from '@/constants/theme';
 import type { HeaderProps as MantineHeaderProps } from '@mantine/core';
 import { Box, Button, Image, Header as MantineHeader, Tooltip } from '@mantine/core';
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
 import { useHeaderStyles } from './styles';
 
 type PrimaryHeaderProps = Omit<MantineHeaderProps, 'children' | 'height' | 'className'>;
@@ -16,9 +15,9 @@ const PrimaryHeader: FC<PrimaryHeaderProps> = (props) => {
 	return (
 		<MantineHeader {...props} className={container} height={HEADER_HEIGHT} p={0} withBorder>
 			<Box className={logoContainer}>
-				<Link to={HOME_ROUTE}>
+				<a href={HOME_ROUTE}>
 					<Image className={imageSty} src={logoInvert} height={32} alt="Parseable Logo" />
-				</Link>
+				</a>
 			</Box>
 			<Box className={navContainer}>
 				<Box
