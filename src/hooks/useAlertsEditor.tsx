8659e049283ba6311214cf15b692d996143c3b4a
edendattox <edendattox@gmail.com>
2024-01-06 00:08:47 +0530

    add useAlertsEditor hook

--- src/hooks/useAlertsEditor.tsx
+++ src/hooks/useAlertsEditor.tsx
@@ -1,33 +1,79 @@
-// import { useState } from 'react';
-// import { useMutation } from 'react-query';
-// import { putLogStreamAlerts } from '@/api/logStream';
+import { useMutation, useQuery } from 'react-query';
+import { getLogStreamAlerts, putLogStreamAlerts } from '@/api/logStream';
+import { IconCheck, IconFileAlert } from '@tabler/icons-react';
+import { notifyApi } from '@/utils/notification';
+import { notifications } from '@mantine/notifications';
+import useMountedState from './useMountedState';
 
-// const useAlertsEditor = (streamName) => {
-//     const [alertQuery, setAlertQuery] = useState('');
+export const useAlertsEditor = (streamName: string) => {
+	const [alertQuery, setAlertQuery] = useMountedState<string | undefined>('');
 
-//     const { mutate: updateLogStreamAlerts } = useMutation(
-//         (data) => putLogStreamAlerts(streamName, data),
-//         {
-//             onSuccess: () => notifyApi({ /* ...success notification details... */ }),
-//             onError: () => notifyApi({ /* ...error notification details... */ }),
-//         },
-//     );
+	const {
+		mutate: updateLogStreamAlerts,
+		data: alertQueryData,
+		isSuccess: updateLogStreamIsSuccess,
+	} = useMutation((data: string) => putLogStreamAlerts(streamName, data), {
+		onSuccess: () => {
+			notifyApi({
+				color: 'green',
+				message: 'Alert Added.',
+				icon: <IconCheck size="1rem" />,
+			});
+		},
+		onError: () => {
+			notifyApi(
+				{
+					color: 'red',
+					title: 'Error occurred',
+					message: `Error occurred, please check your query and try again`,
+					icon: <IconFileAlert size="1rem" />,
+					autoClose: 3000,
+				},
+				true,
+			);
+		},
+	});
 
-//     const handleAlertQueryChange = (value) => setAlertQuery(value);
+	const { data: getLogAlertData } = useQuery(
+		['fetch-log-stream-alert', streamName, updateLogStreamIsSuccess],
+		() => getLogStreamAlerts(streamName),
+		{
+			onError: () => {
+				notifyApi({
+					color: 'red',
+					message: 'Failed to log streams alert',
+					icon: <IconFileAlert size="1rem" />,
+				});
+			},
+			retry: false,
+			enabled: streamName !== '',
+		},
+	);
 
-//     const submitAlertQuery = () => {
-//         try {
-//             JSON.parse(alertQuery);
-//             updateLogStreamAlerts(alertQuery);
-//         } catch (e) {
-//             notifyApi({ /* ...error notification details... */ });
-//         }
-//     };
+	const handleAlertQueryChange = (value: string | undefined) => setAlertQuery(value);
 
-//     return {
-//         alertQuery,
-//         handleAlertQueryChange,
-//         submitAlertQuery,
-//         logAlertData: /* ...data from API or state... */,
-//     };
-// };
+	const submitAlertQuery = () => {
+		try {
+			JSON.parse(alertQuery!);
+			updateLogStreamAlerts(alertQuery!);
+		} catch (e) {
+			notifications.show({
+				id: 'load-data',
+				loading: false,
+				color: 'red',
+				title: 'Error occurred',
+				message: `Error occurred, please check your query and try again ${e}`,
+				icon: <IconFileAlert size="1rem" />,
+				autoClose: 3000,
+			});
+		}
+	};
+
+	return {
+		alertQuery,
+		handleAlertQueryChange,
+		submitAlertQuery,
+		alertQueryData,
+		getLogAlertData,
+	};
+};
