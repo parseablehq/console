1f228ca032a8ccce7bd184ae562d922c3b265e1e
Balaji <balaji.joshua1998@gmail.com>
2024-06-07 13:11:10 +0530

    support for case insensitive filter for text fields (#249)
    * support for case insensitive filter for text fields
    
    * rearrange operators in the list
    
    * updated labels for text field operators

--- src/pages/Stream/providers/FilterProvider.tsx
+++ src/pages/Stream/providers/FilterProvider.tsx
@@ -4,8 +4,9 @@ import initContext from '@/utils/initContext';
 import { Field, RuleGroupType, RuleType, formatQuery } from 'react-querybuilder';
 
 export const textFieldOperators = [
-	{ value: '=', label: '=' },
-	{ value: '!=', label: '!=' },
+	{ value: '=', label: 'equals to' },
+	{ value: '!=', label: 'not equals to' },
+	{ value: 'ilike', label: 'case-insensitive match' },
 	{ value: 'contains', label: 'contains' },
 	{ value: 'beginsWith', label: 'begins with' },
 	{ value: 'endsWith', label: 'ends with' },
