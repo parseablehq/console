cfdae77cc54486b4de2d1b3fd3bf192bc51c2eb7
balaji-jr <balaji.joshua1998@gmail.com>
2024-06-24 12:39:53 +0530

    replace limit in custom query if it exceeds load limit

--- src/utils/sanitiseSqlString.ts
+++ src/utils/sanitiseSqlString.ts
@@ -2,13 +2,23 @@ import { notify } from './notification';
 import { LOAD_LIMIT } from '@/pages/Stream/providers/LogsProvider';
 
 export const sanitiseSqlString = (sqlString: string): string => {
-	const withoutComments = sqlString.replace(/--.*$/gm, '');
-	const withoutNewLines = withoutComments.replace(/\n/g, ' ');
-	const withoutTrailingSemicolon = withoutNewLines.replace(/;/, '');
-	const limitRegex = /limit\s+(\d+)/i;
-	if (!limitRegex.test(withoutTrailingSemicolon)) {
-		notify({ message: `Default limit used i.e - ${LOAD_LIMIT}` });
-		return `${withoutTrailingSemicolon.trim()} LIMIT ${LOAD_LIMIT}`;
-	}
-	return withoutTrailingSemicolon;
+    const withoutComments = sqlString.replace(/--.*$/gm, '');
+    const withoutNewLines = withoutComments.replace(/\n/g, ' ');
+    const withoutTrailingSemicolon = withoutNewLines.replace(/;/g, '').trim();
+    const limitRegex = /limit\s+(\d+)/i;
+
+    if (limitRegex.test(withoutTrailingSemicolon)) {
+        return withoutTrailingSemicolon.replace(limitRegex, (match, p1) => {
+            const currentLimit = parseInt(p1, 10);
+			console.log(currentLimit, LOAD_LIMIT)
+            if (currentLimit > LOAD_LIMIT) {
+                notify({ message: `Limit exceeds the default load limit. Replacing with default limit - ${LOAD_LIMIT}` });
+                return `LIMIT ${LOAD_LIMIT}`;
+            }
+            return match;
+        });
+    }
+
+    notify({ message: `Default limit used i.e - ${LOAD_LIMIT}` });
+    return `${withoutTrailingSemicolon} LIMIT ${LOAD_LIMIT}`;
 };
