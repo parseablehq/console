3949493b4ea4f50b7127eda59ab7042a521443b0
balaji-jr <balaji.joshua1998@gmail.com>
2024-06-30 18:33:24 +0530

    basic nav setup for dashboards

---
+++ src/pages/Dashboards/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mantine/core';
+
+const Dashboards = () => {
+	return (
+		<Box style={{ flex: 1, display: 'flex', position: 'relative', flexDirection: 'row', width: '100%', border: '1px solid' }}>
+			{/* <div style={{ border: '1px solid', height: '100%', width: '100%' }}>
+			</div> */}
+		</Box>
+	);
+};
+
+export default Dashboards;
