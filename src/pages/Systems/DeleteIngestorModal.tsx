9749d16a3b7d94b2b3e6d42b7f8bb13363f3ba14
Koustav <kdkdas8@gmail.com>
2024-07-15 16:18:11 +0530

    optimised

--- src/pages/Systems/DeleteIngestorModal.tsx
+++ src/pages/Systems/DeleteIngestorModal.tsx
@@ -3,6 +3,7 @@ import { useClusterInfo, useDeleteIngestor } from '@/hooks/useClusterInfo';
 import { useClusterStore, clusterStoreReducers } from './providers/ClusterProvider';
 import classes from './styles/Systems.module.css';
 import { useCallback } from 'react';
+import { machine } from 'os';
 
 function sanitizeIngestorUrl(url: string) {
 	if (url.startsWith('http://')) {
@@ -28,6 +29,7 @@ export default function DeleteIngestorModal(props: { modalOpened: boolean; close
 	const { deleteIngestorMutation, deleteIngestorIsLoading } = useDeleteIngestor();
 	const { getClusterInfoRefetch } = useClusterInfo();
 	const [ingestorAddress, setClusterStore] = useClusterStore((store) => store.currentMachine);
+	const [ingestor] = useClusterStore((store) => store.currentMachineType);
 
 	const onDeleteIngestorSuccess = useCallback(() => {
 		props.closeModal();
@@ -42,7 +44,7 @@ export default function DeleteIngestorModal(props: { modalOpened: boolean; close
 			ingestorUrl: sanitizeIngestorUrl(ingestorAddress),
 			onSuccess: onDeleteIngestorSuccess,
 		});
-	}, [ingestorAddress]);
+	}, [ingestorAddress, ingestor]);
 
 	return (
 		<Modal
