a889cb44c7b67ea10617200c454a26172b18ab7e
Koustav <kdkdas8@gmail.com>
2024-07-15 16:04:44 +0530

    optimised

--- src/pages/Systems/DeleteIngestorModal.tsx
+++ src/pages/Systems/DeleteIngestorModal.tsx
@@ -42,7 +42,7 @@ export default function DeleteIngestorModal(props: { modalOpened: boolean; close
 			ingestorUrl: sanitizeIngestorUrl(ingestorAddress),
 			onSuccess: onDeleteIngestorSuccess,
 		});
-	}, [ingestorAddress, onDeleteIngestorSuccess]);
+	}, [ingestorAddress]);
 
 	return (
 		<Modal
