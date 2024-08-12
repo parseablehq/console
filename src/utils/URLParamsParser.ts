d2bf1c008ca67b5ce3d81a12db10637a0611267a
Koustav <kdkdas8@gmail.com>
2024-08-12 14:07:36 +0530

    URL parser can parse to multiple query params

--- src/utils/URLParamsParser.ts
+++ src/utils/URLParamsParser.ts
@@ -1,6 +1,14 @@
 export function paramsParser(queryParams: Record<string, string>) {
-	for (let i in queryParams) {
-		const parsedParams = `${i}=${queryParams[i]}`;
-		return parsedParams;
+	let parsedParams = '';
+
+	for (const i in queryParams) {
+		if (queryParams.hasOwnProperty(i)) {
+			parsedParams += `${i}=${queryParams[i]}&`;
+		}
 	}
+
+	if (parsedParams.endsWith('&')) {
+		parsedParams = parsedParams.slice(0, -1);
+	}
+	return parsedParams;
 }
