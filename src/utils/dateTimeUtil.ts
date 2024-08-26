63fc52d84186c38043210feddecbfceec7fbc720
Koustav <kdkdas8@gmail.com>
2024-08-26 17:23:28 +0530

    changed to a 12 hour clock'

--- src/utils/dateTimeUtil.ts
+++ src/utils/dateTimeUtil.ts
@@ -2,12 +2,13 @@ import moment from 'moment-timezone';
 import _ from 'lodash';
 
 //accepts a date-time string and outputs a human readable string with timezone
+//output format 31/12/1990 11:59 pm IST
 const getDateTimeWithTZ = (dateTime: string | undefined) => {
 	if (!dateTime && typeof dateTime !== 'string' && _.isEmpty(dateTime)) return;
 
 	const sysTimeZone = moment.tz.guess();
 	const convertedDate = moment.tz(dateTime, sysTimeZone);
-	return convertedDate.format('DD MMM YYYY HH:mm z');
+	return convertedDate.format('DD/MM/YYYY h:mm a z');
 };
 
 const dateTimeUtils = { getDateTimeWithTZ };
