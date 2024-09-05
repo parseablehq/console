d91bef2a4de1234e227536c1acea0b097ee725ab
balaji-jr <balaji.joshua1998@gmail.com>
2024-09-05 12:06:24 +0530

    implement scroll view for cluster page sidebar

--- src/pages/Systems/Cluster.tsx
+++ src/pages/Systems/Cluster.tsx
@@ -3,12 +3,20 @@ import classes from './styles/Systems.module.css';
 import _ from 'lodash';
 import ServerList from './ServerList';
 import ServerDetail from './ServerDetails';
+import { PRIMARY_HEADER_HEIGHT } from '@/constants/theme';
 
 const Cluster = () => {
 	return (
-		<Stack className={classes.container} w="100%">
-			<Stack style={{ flexDirection: 'row', width: '100%' }} gap={0}>
-				<Stack style={{ width: '20%' }}>
+		<Stack
+			className={classes.container}
+			w="100%"
+			style={{
+				maxHeight: `calc(100vh - ${PRIMARY_HEADER_HEIGHT}px )`,
+				overflowY: 'scroll',
+				flex: 1,
+			}}>
+			<Stack style={{ flexDirection: 'row', width: '100%', overflow: 'hidden' }} gap={0}>
+				<Stack style={{ width: '20%', height: '100%' }}>
 					<ServerList />
 				</Stack>
 				<Divider orientation="vertical" />
