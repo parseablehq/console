ce3c5e625cce8c4a434539f39378142796db96b9
Koustav Das <78158736+Koustavd18@users.noreply.github.com>
2024-11-06 15:45:18 +0530

    Udpate Title based on page (#356)

--- src/pages/Systems/index.tsx
+++ src/pages/Systems/index.tsx
@@ -3,8 +3,10 @@ import { FC } from 'react';
 import { useAbout } from '@/hooks/useGetAbout';
 import Cluster from './Cluster';
 import { useAppStore } from '@/layouts/MainLayout/providers/AppProvider';
+import { useDocumentTitle } from '@mantine/hooks';
 
 const Systems: FC = () => {
+	useDocumentTitle('Parseable | Cluster');
 	const { getAboutIsLoading, getAboutIsError } = useAbout();
 	const [isStandAloneMode] = useAppStore((store) => store.isStandAloneMode);
 	if (getAboutIsLoading || getAboutIsError || isStandAloneMode !== false) return null;
