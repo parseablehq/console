66b823782c1b85b19666a4b3baca85ee432dd89a
Praveen K B <praveenbhat98@gmail.com>
2024-11-13 14:04:47 +0530

    Added sidebar and collapsable config

--- src/pages/Correlation/index.tsx
+++ src/pages/Correlation/index.tsx
@@ -1,55 +1,73 @@
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 import _ from 'lodash';
-import { useDocumentTitle } from '@mantine/hooks';
-import { Stack } from '@mantine/core';
+import { useDisclosure, useDocumentTitle } from '@mantine/hooks';
+import { Stack, Box, Group, Button, Collapse } from '@mantine/core';
 import LogsView from '../Stream/Views/Explore/LogsView';
 import Querier from '../Stream/components/Querier';
 import SecondaryToolbar from '../Stream/components/SecondaryToolbar';
 import {
 	PRIMARY_HEADER_HEIGHT,
+	SECONDARY_SIDEBAR_WIDTH,
 	STREAM_PRIMARY_TOOLBAR_CONTAINER_HEIGHT,
 	STREAM_PRIMARY_TOOLBAR_HEIGHT,
 } from '@/constants/theme';
-import { MaximizeButton, SavedFiltersButton } from '../Stream/components/PrimaryToolbar';
+import { MaximizeButton } from '../Stream/components/PrimaryToolbar';
 import TimeRange from '@/components/Header/TimeRange';
 import RefreshInterval from '@/components/Header/RefreshInterval';
 import RefreshNow from '@/components/Header/RefreshNow';
 import ShareButton from '@/components/Header/ShareButton';
+import SideBar from '@/components/SideBar/Sidebar';
 
 const Correlation = () => {
 	useDocumentTitle('Parseable | Correlation');
-
+	const [opened, { toggle }] = useDisclosure(false);
+	const sideBarWidth = SECONDARY_SIDEBAR_WIDTH;
 	return (
-		<Stack
-			gap={0}
+		<Box
 			style={{
-				maxHeight: `calc(100vh - ${PRIMARY_HEADER_HEIGHT}px )`,
-				overflowY: 'scroll',
 				flex: 1,
+				display: 'flex',
+				position: 'relative',
+				flexDirection: 'row',
+				width: '100%',
 			}}>
-			<h1>PKB</h1>
-			<Stack
-				style={{
-					height: STREAM_PRIMARY_TOOLBAR_CONTAINER_HEIGHT,
-					alignItems: 'center',
-					justifyContent: 'center',
-					padding: '0 1.25rem',
-				}}>
-				<Stack style={{ flexDirection: 'row', height: STREAM_PRIMARY_TOOLBAR_HEIGHT }} w="100%">
-					<Querier />
-					<SavedFiltersButton />
-					<TimeRange />
-					<RefreshInterval />
-					<RefreshNow />
-					{/* <ViewToggle /> */}
-					<ShareButton />
-					<MaximizeButton />
-				</Stack>
+			<Stack style={{ width: sideBarWidth }}>
+				<SideBar />
 			</Stack>
-			<SecondaryToolbar />
-			<LogsView schemaLoading={false} infoLoading={false} />
-		</Stack>
+			<Stack
+				gap={0}
+				style={{
+					maxHeight: `calc(100vh - ${PRIMARY_HEADER_HEIGHT}px )`,
+					overflowY: 'scroll',
+					flex: 1,
+				}}>
+				<Group justify="center" mb={5}>
+					<Button onClick={toggle}>Config</Button>
+				</Group>
+				<Collapse in={opened}>
+					<h1>PKB</h1>
+				</Collapse>
+				<Stack
+					style={{
+						height: STREAM_PRIMARY_TOOLBAR_CONTAINER_HEIGHT,
+						alignItems: 'center',
+						justifyContent: 'center',
+						padding: '0 1.25rem',
+					}}>
+					<Stack style={{ flexDirection: 'row', height: STREAM_PRIMARY_TOOLBAR_HEIGHT }} w="100%">
+						<Querier />
+						<TimeRange />
+						<RefreshInterval />
+						<RefreshNow />
+						<ShareButton />
+						<MaximizeButton />
+					</Stack>
+				</Stack>
+				<SecondaryToolbar />
+				<LogsView schemaLoading={false} infoLoading={false} />
+			</Stack>
+		</Box>
 	);
 };
 
