9414cae6b1e58d3989e90bee27cb2c2169921aa0
Koustav <kdkdas8@gmail.com>
2025-01-20 20:33:00 +0530

    Itertion 2"

--- src/pages/Alerts/CreateAlerts.tsx
+++ src/pages/Alerts/CreateAlerts.tsx
@@ -5,61 +5,28 @@ import {
 	Stack,
 	Text,
 	TextInput,
+	Textarea,
 	Box,
 	Breadcrumbs,
 	Anchor,
-	Divider,
+	// Divider,
 	Group,
-	ThemeIcon,
-	ScrollArea,
+	// ThemeIcon,
+	// ScrollArea,
 	FloatingIndicator,
 	UnstyledButton,
 } from '@mantine/core';
 import classes from './styles/AlertStyles.module.css';
 import { useDocumentTitle } from '@mantine/hooks';
 import { IconPlus } from '@tabler/icons-react';
-import { IconPhoto, IconPrinter, IconCameraSelfie } from '@tabler/icons-react';
-import { Accordion } from '@mantine/core';
 
-function Demo() {
-	return (
-		<Accordion variant="contained" multiple={true} defaultValue={['rules']}>
-			<Accordion.Item value="rules">
-				<Accordion.Control icon={<IconPhoto size={20} color="var(--mantine-color-blue-6)" />}>
-					Create Rules
-				</Accordion.Control>
-				<Accordion.Panel>
-					{' '}
-					<Stack>
-						<AlertBuilder />
-					</Stack>
-				</Accordion.Panel>
-			</Accordion.Item>
+// type CombinatorToggleType = {
+// 	isOrSelected: boolean;
+// 	// onCombinatorChange: (combinator: Combinator) => void;
+// };
 
-			<Accordion.Item value="print">
-				<Accordion.Control icon={<IconPrinter size={20} color="var(--mantine-color-blue-6)" />}>
-					Print photos
-				</Accordion.Control>
-				<Accordion.Panel>Content</Accordion.Panel>
-			</Accordion.Item>
-
-			<Accordion.Item value="target">
-				<Accordion.Control icon={<IconCameraSelfie size={20} color="var(--mantine-color-teal-6)" />}>
-					Add Target
-				</Accordion.Control>
-				<Accordion.Panel>Content</Accordion.Panel>
-			</Accordion.Item>
-		</Accordion>
-	);
-}
-
-type CombinatorToggleType = {
-	isOrSelected: boolean;
-	// onCombinatorChange: (combinator: Combinator) => void;
-};
-
-const activeBtnClass = `${classes.toggleBtnText} ${classes.toggleBtnActive}`;
-const inActiveBtnClass = classes.toggleBtnText;
+// const activeBtnClass = `${classes.toggleBtnText} ${classes.toggleBtnActive}`;
+// const inActiveBtnClass = classes.toggleBtnText;
 
 const items = [
 	{
@@ -76,7 +43,7 @@ const items = [
 
 const AggregateOptions = ['AVG', 'COUNT', 'MAX', 'MIN', 'SUM'];
 
-const SeverityLevels = ['Low(P3)', 'Medium(P2)', 'High(P1)', 'Critical(P0)'];
+// const SeverityLevels = ['Low(P3)', 'Medium(P2)', 'High(P1)', 'Critical(P0)'];
 
 const data = ['Threshold', 'Rate', 'Type3', 'Type4'];
 
@@ -117,145 +84,186 @@ const PageTitle = () => {
 	return <Text size="lg">New Alert</Text>;
 };
 
-const CombinatorToggle = (props: CombinatorToggleType) => {
-	const { isOrSelected } = props;
+// const CombinatorToggle = (props: CombinatorToggleType) => {
+// 	const { isOrSelected } = props;
+// 	return (
+// 		<Box className={classes.toggleBtnContainer}>
+// 			<Text
+// 				style={{ fontSize: '0.6rem' }}
+// 				className={isOrSelected ? activeBtnClass : inActiveBtnClass}
+// 				onClick={() => console.log('hello world')}>
+// 				OR
+// 			</Text>
+// 			<Text
+// 				style={{ fontSize: '0.6rem' }}
+// 				className={!isOrSelected ? activeBtnClass : inActiveBtnClass}
+// 				onClick={() => console.log('and')}>
+// 				AND
+// 			</Text>
+// 		</Box>
+// 	);
+// };
+// const AddRuleGroupBtn = () => {
+// 	return (
+// 		<Stack
+// 			h="12rem"
+// 			className={classes.rulesContainer}
+// 			style={{ cursor: 'pointer' }}
+// 			onClick={() => console.log('adding rules')}>
+// 			<Stack style={{ flexDirection: 'row' }} align="center" gap={8}>
+// 				<ThemeIcon radius="lg" size="sm" p={4}>
+// 					<IconPlus stroke={3} />
+// 				</ThemeIcon>
+// 				<Text size="md" fw={600}>
+// 					Add
+// 				</Text>
+// 			</Stack>
+// 		</Stack>
+// 	);
+// };
+
+// const ConditionBox = () => {
+// 	return (
+// 		<div
+// 			className={classes.rulesContainer}
+// 			style={{
+// 				width: '78%',
+// 				position: 'relative',
+// 			}}>
+// 			<span
+// 				className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white px-2 text-sm text-gray-600"
+// 				style={{
+// 					position: 'absolute',
+// 					top: '-5.8%',
+// 					left: '45%',
+// 					background: '#f5f5f5',
+// 					padding: '0rem 1rem',
+// 					color: '#4b52ea',
+// 					fontWeight: 700,
+// 				}}>
+// 				Condition
+// 			</span>
+// 			<Box w="100%" style={{ display: 'flex', justifyContent: 'end' }}>
+// 				<Box className={classes.parentCombinatorToggleContainer}>
+// 					<CombinatorToggle isOrSelected={false} />
+// 				</Box>
+// 			</Box>
+// 			<Group gap={10}>
+// 				<Select w="32.5%" label="Field" />
+// 				<Select w="31%" label="Operator" />
+// 				<TextInput w="33%" label="Value" />
+// 				<Button leftSection={<IconPlus stroke={1.2} size={'1rem'} />}>Condition</Button>
+// 			</Group>
+// 		</div>
+// 	);
+// };
+
+// function AlertBuilder() {
+// 	return (
+// 		<Stack h="40vh" style={{ justifyContent: 'center', alignItems: 'center' }}>
+// 			<ScrollArea
+// 				w="65%"
+// 				scrollbars="y"
+// 				type="always"
+// 				scrollbarSize={12}
+// 				px={16}
+// 				py={20}
+// 				style={{ background: '#f5f5f5', borderRadius: 20 }}
+// 				styles={{ thumb: { backgroundColor: '#4b52ea' } }}>
+// 				<Stack style={{ justifyContent: 'center', gap: 0 }}>
+// 					<Stack className={classes.rulesContainer}>
+// 						<Select
+// 							w="12%"
+// 							label="Aggregate"
+// 							defaultValue={AggregateOptions[0]}
+// 							data={AggregateOptions}
+// 							styles={{ label: { color: '#4b52ea', fontWeight: 700 } }}
+// 						/>
+
+// 						<ConditionBox />
+
+// 						<Select w="6%" label="Operator" styles={{ label: { color: '#4b52ea', fontWeight: 700 } }} />
+// 						<TextInput w="10%" label="Value" styles={{ label: { color: '#4b52ea', fontWeight: 700 } }} />
+// 					</Stack>
+// 					<Stack style={{ height: 80, position: 'relative' }}>
+// 						<Stack className={classes.ruleSetConnector} />
+// 						<Stack style={{ position: 'absolute', height: 80, alignItems: 'center', justifyContent: 'center' }}>
+// 							<Stack className={classes.parentCombinatorToggleContainer}>
+// 								<CombinatorToggle
+// 									isOrSelected={false}
+// 									// onCombinatorChange={onParentCombinatorChange}
+// 								/>
+// 							</Stack>
+// 						</Stack>
+// 					</Stack>
+// 					<AddRuleGroupBtn />
+// 				</Stack>
+// 				<Stack pt={20}>
+// 					<Divider label="Target" />
+// 					<Group>
+// 						<TextInput label="Target" w="100%" />
+// 						<Box w="100%" style={{ display: 'flex', justifyContent: 'end', gap: 10 }}>
+// 							<Button variant="outline">Edit</Button>
+// 							<Button>Save </Button>
+// 						</Box>
+// 					</Group>
+// 					<Divider label="Evalution" />
+// 					<Group>
+// 						<TextInput w="49%" label="Time" placeholder="Start Time" />
+// 						<TextInput w="49%" label="Frequency" placeholder="10s" />
+// 					</Group>
+// 					<Divider label="Severity" />
+// 					<Group>
+// 						<Select
+// 							label="Severity"
+// 							size="sm"
+// 							w="15rem"
+// 							defaultValue={SeverityLevels[1]}
+// 							data={SeverityLevels}></Select>
+// 					</Group>
+// 				</Stack>
+// 			</ScrollArea>
+// 		</Stack>
+// 	);
+// }
+
+function AlertsForm() {
 	return (
-		<Box className={classes.toggleBtnContainer}>
-			<Text
-				style={{ fontSize: '0.6rem' }}
-				className={isOrSelected ? activeBtnClass : inActiveBtnClass}
-				onClick={() => console.log('hello world')}>
-				OR
-			</Text>
-			<Text
-				style={{ fontSize: '0.6rem' }}
-				className={!isOrSelected ? activeBtnClass : inActiveBtnClass}
-				onClick={() => console.log('and')}>
-				AND
-			</Text>
-		</Box>
-	);
-};
-const AddRuleGroupBtn = () => {
-	return (
-		<Stack
-			h="12rem"
-			className={classes.rulesContainer}
-			style={{ cursor: 'pointer' }}
-			onClick={() => console.log('adding rules')}>
-			<Stack style={{ flexDirection: 'row' }} align="center" gap={8}>
-				<ThemeIcon radius="lg" size="sm" p={4}>
-					<IconPlus stroke={3} />
-				</ThemeIcon>
-				<Text size="md" fw={600}>
-					Add
-				</Text>
+		<Stack>
+			{/* rules */}
+			<Stack>
+				<Text style={{ color: '#4b52ea', fontWeight: 700, fontSize: 14 }}>Condition</Text>
+				<Group w={'50%'} gap={10}>
+					<Select w="32.5%" label="Field" />
+					<Select w="31%" label="Operator" />
+					<TextInput w="33%" label="Value" />
+					<Button leftSection={<IconPlus stroke={1.2} size={'1rem'} />}>Condition</Button>
+				</Group>
 			</Stack>
-		</Stack>
-	);
-};
+			{/* eval */}
+			<Stack ml={'2%'} mt="1%" style={{ borderLeft: '2px solid #4B52EA', padding: '1rem' }}>
+				<Text style={{ color: '#4b52ea', fontWeight: 700, fontSize: 14 }}>Evalution Details</Text>
+				<Group>
+					<Select w="12%" label="Aggregate" defaultValue={AggregateOptions[0]} data={AggregateOptions} />
+					<Select w="6%" label="Operator" styles={{ label: { color: '#4b52ea', fontWeight: 700 } }} />
+					<TextInput w="10%" label="Value" styles={{ label: { color: '#4b52ea', fontWeight: 700 } }} />
+				</Group>
+				<Group>
+					<Select w="12%" label="Window" defaultValue={'Window'} data={['Window']} />
+					<Select w="6%" label="Frequency" styles={{ label: { color: '#4b52ea', fontWeight: 700 } }} />
+				</Group>
+			</Stack>
 
-const ConditionBox = () => {
-	return (
-		<div
-			className={classes.rulesContainer}
-			style={{
-				width: '78%',
-				position: 'relative',
-			}}>
-			<span
-				className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white px-2 text-sm text-gray-600"
-				style={{
-					position: 'absolute',
-					top: '-5.8%',
-					left: '45%',
-					background: '#f5f5f5',
-					padding: '0rem 1rem',
-					color: '#4b52ea',
-					fontWeight: 700,
-				}}>
-				Condition
-			</span>
-			<Box w="100%" style={{ display: 'flex', justifyContent: 'end' }}>
-				<Box className={classes.parentCombinatorToggleContainer}>
-					<CombinatorToggle isOrSelected={false} />
-				</Box>
-			</Box>
-			<Group gap={10}>
-				<Select w="32.5%" label="Field" />
-				<Select w="31%" label="Operator" />
-				<TextInput w="33%" label="Value" />
-				<Button leftSection={<IconPlus stroke={1.2} size={'1rem'} />}>Condition</Button>
-			</Group>
-		</div>
-	);
-};
-
-function AlertBuilder() {
-	return (
-		<Stack h="40vh" style={{ justifyContent: 'center', alignItems: 'center' }}>
-			<ScrollArea
-				w="65%"
-				scrollbars="y"
-				type="always"
-				scrollbarSize={12}
-				px={16}
-				py={20}
-				style={{ background: '#f5f5f5', borderRadius: 20 }}
-				styles={{ thumb: { backgroundColor: '#4b52ea' } }}>
-				<Stack style={{ justifyContent: 'center', gap: 0 }}>
-					<Stack className={classes.rulesContainer}>
-						<Select
-							w="12%"
-							label="Aggregate"
-							defaultValue={AggregateOptions[0]}
-							data={AggregateOptions}
-							styles={{ label: { color: '#4b52ea', fontWeight: 700 } }}
-						/>
-
-						<ConditionBox />
-
-						<Select w="6%" label="Operator" styles={{ label: { color: '#4b52ea', fontWeight: 700 } }} />
-						<TextInput w="10%" label="Value" styles={{ label: { color: '#4b52ea', fontWeight: 700 } }} />
-					</Stack>
-					<Stack style={{ height: 80, position: 'relative' }}>
-						<Stack className={classes.ruleSetConnector} />
-						<Stack style={{ position: 'absolute', height: 80, alignItems: 'center', justifyContent: 'center' }}>
-							<Stack className={classes.parentCombinatorToggleContainer}>
-								<CombinatorToggle
-									isOrSelected={false}
-									// onCombinatorChange={onParentCombinatorChange}
-								/>
-							</Stack>
-						</Stack>
-					</Stack>
-					<AddRuleGroupBtn />
-				</Stack>
-				<Stack pt={20}>
-					<Divider label="Target" />
-					<Group>
-						<TextInput label="Target" w="100%" />
-						<Box w="100%" style={{ display: 'flex', justifyContent: 'end', gap: 10 }}>
-							<Button variant="outline">Edit</Button>
-							<Button>Save </Button>
-						</Box>
-					</Group>
-					<Divider label="Evalution" />
-					<Group>
-						<TextInput w="49%" label="Time" placeholder="Start Time" />
-						<TextInput w="49%" label="Frequency" placeholder="10s" />
-					</Group>
-					<Divider label="Severity" />
-					<Group>
-						<Select
-							label="Severity"
-							size="sm"
-							w="15rem"
-							defaultValue={SeverityLevels[1]}
-							data={SeverityLevels}></Select>
-					</Group>
-				</Stack>
-			</ScrollArea>
+			{/* Target */}
+			<Stack mt="1%">
+				<Text style={{ color: '#4b52ea', fontWeight: 700, fontSize: 14 }}>Target</Text>
+				<Select w="12%" label="Type" defaultValue={'Slack'} data={['Slack', 'Webhook']} />
+				<TextInput w="40%" label="Target" />
+				<Button w="5%" leftSection={<IconPlus stroke={1.2} size={'1rem'} />}>
+					Target
+				</Button>
+				<Textarea label="Message" w="40%" />
+			</Stack>
 		</Stack>
 	);
 }
@@ -270,11 +278,12 @@ export default function CreateAlerts() {
 			<SelectStream />
 			<TypeSelect />
 
-			<Demo />
+			<Box h={'100%'} className={classes.alertFormContainer}>
+				<AlertsForm />
+			</Box>
 
 			<Box style={{ display: 'flex', justifyContent: 'end', gap: 10 }}>
-				<Button variant="outline">Discard</Button>
-				<Button>Save Alert</Button>
+				<Button variant="outline">Set Alert</Button>
 			</Box>
 		</Stack>
 	);
