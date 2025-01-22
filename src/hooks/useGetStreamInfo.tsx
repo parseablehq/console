5dc6dbee73a0d6a1b3fff835139f756ab841c79c
Praveen K B <praveenbhat98@gmail.com>
2025-01-22 16:11:17 +0530

    fix: Avoid info refetch in manage page

--- src/hooks/useGetStreamInfo.tsx
+++ src/hooks/useGetStreamInfo.tsx
@@ -18,7 +18,7 @@ export const useGetStreamInfo = (currentStream: string, initialFetch: boolean) =
 	} = useQuery(['stream-info', currentStream], () => getLogStreamInfo(currentStream), {
 		retry: false,
 		refetchOnWindowFocus: false,
-		refetchOnMount: true,
+		refetchOnMount: false,
 		enabled: initialFetch,
 		onSuccess: (data) => {
 			setStreamStore((store) => setStreamInfo(store, data));
